cc47f1c592dbfefc0c5581dd35cb6cca89a42191
Marcelo Rangel <Marcelo.rcs@icloud.com>
2019-12-05 09:45:34 -0300

    Adicionando Login Page + Login Start + SplashScreen

---
+++ App/app/src/main/java/br/senac/go/app/SplashScreen.java
@@ -0,0 +1,37 @@
+package br.senac.go.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashScreen.this,LoginStart.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
