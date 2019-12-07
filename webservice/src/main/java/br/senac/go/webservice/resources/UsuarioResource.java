2afac4b312697ac9e7f46ffdc4256bc639afa0b2
allas <allassantos1000@gmail.com>
2019-12-07 15:25:30 -0300

    Criando esqueleto do projeto parte 3

--- webservice/src/main/java/br/senac/go/webservice/resources/UsuarioResource.java
+++ webservice/src/main/java/br/senac/go/webservice/resources/UsuarioResource.java
@@ -1,10 +1,14 @@
 package br.senac.go.webservice.resources;
 
+import br.senac.go.webservice.error.CustomErrorType;
+import br.senac.go.webservice.model.Usuario;
 import br.senac.go.webservice.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -16,8 +20,33 @@ public class UsuarioResource {
     public UsuarioResource(UsuarioRepository usuarioDAO) {
         this.usuarioDAO = usuarioDAO;
     }
+
     @GetMapping
     public ResponseEntity<?> listAll(){
         return new ResponseEntity<>(usuarioDAO.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getUsuarioById(@PathVariable("id") Long id){
+        Optional<Usuario> usuario = usuarioDAO.findById(id);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Usuario usuario){
+        return new ResponseEntity<>(usuarioDAO.save(usuario), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        usuarioDAO.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Usuario usuario){
+        usuarioDAO.save(usuario);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- webservice/src/main/java/br/senac/go/webservice/resources/VeiculoResource.java
+++ webservice/src/main/java/br/senac/go/webservice/resources/VeiculoResource.java
@@ -1,12 +1,14 @@
 package br.senac.go.webservice.resources;
 
+import br.senac.go.webservice.model.Usuario;
+import br.senac.go.webservice.model.Veiculo;
 import br.senac.go.webservice.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/veiculo")
@@ -21,5 +23,28 @@ public class VeiculoResource {
     @GetMapping
     public ResponseEntity<?> listAll(){
         return new ResponseEntity<>(veiculoDAO.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getVeiculoById(@PathVariable("id") Long id){
+        Optional<Veiculo> veiculo = veiculoDAO.findById(id);
+        return new ResponseEntity<>(veiculo, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Veiculo veiculo){
+        return new ResponseEntity<>(veiculoDAO.save(veiculo), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        veiculoDAO.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Veiculo veiculo){
+        veiculoDAO.save(veiculo);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
