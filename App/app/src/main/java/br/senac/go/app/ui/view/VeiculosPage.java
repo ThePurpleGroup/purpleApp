f4097dbc9d44f09534f41dc63844ef94a2d0372a
Marcelo Rangel <Marcelo.rcs@icloud.com>
2019-12-08 00:37:46 -0300

    Consulta de Veiculos pela id_usuario

--- App/app/src/main/java/br/senac/go/app/ui/view/VeiculosPage.java
+++ App/app/src/main/java/br/senac/go/app/ui/view/VeiculosPage.java
@@ -6,17 +6,32 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import br.senac.go.app.R;
+import br.senac.go.app.data.model.Usuario;
 import br.senac.go.app.data.model.Veiculo;
+import br.senac.go.app.data.repository.Callback;
+import br.senac.go.app.data.repository.IVeiculoRepository;
+import br.senac.go.app.data.repository.VeiculoRepository;
+import br.senac.go.app.data.repository.source.VeiculoAPISource;
 import br.senac.go.app.ui.adapter.VeiculoAdapter;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class VeiculosPage extends AppCompatActivity implements VeiculoAdapter.OnVeiculoListener {
+public class VeiculosPage extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private IVeiculoRepository veiculoRepository;
+
+
+    private static final String TAG = "VeiculosPage";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +40,47 @@ public class VeiculosPage extends AppCompatActivity implements VeiculoAdapter.On
 
         recyclerView = findViewById(R.id.recycler_veiculos);
 
-        List<Veiculo> veiculoList = new ArrayList<>();
+        Retrofit retrofit = new Retrofit
+                .Builder()
+                .baseUrl("http://192.168.1.34:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        VeiculoAPISource veiculoAPISource = retrofit.create(VeiculoAPISource.class);
 
-        for (int i = 0; i < 30; i++) {
-            Veiculo veiculo = new Veiculo();
-            Long id = new Long(i);
-            veiculo.setId(id);
-            veiculo.setDescricao("Carro" + i);
-            veiculo.setPlaca("SNC-00" + i);
-            veiculo.setTipo("Nicholas");
+        veiculoRepository = new VeiculoRepository(veiculoAPISource);
 
-            veiculoList.add(veiculo);
-
-        }
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.setAdapter(new VeiculoAdapter(getApplicationContext(), veiculoList));
 
     }
 
-
     @Override
-    public void onVeiculoClick(int position) {
-        Intent intent = new Intent(this, AbastecimentosPage.class);
-        startActivity(intent);
+    protected void onStart() {
+        super.onStart();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(3L);
+
+        veiculoRepository.getVeiculosUsuario(new Callback<List<Veiculo>>() {
+            @Override
+            public void onResult(List<Veiculo> result) {
+                RecyclerView.Adapter adapter = new VeiculoAdapter(getApplicationContext(),result);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("mainAct", "Erro ao buscar lista de carros", e);
+            }
+
+            @Override
+            public void onEmpty() {
+                recyclerView.setAdapter(null);
+            }
+        }, usuario);
+
+
     }
 }
 
